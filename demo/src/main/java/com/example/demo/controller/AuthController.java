f146941b57856209b19c64d22ca5caaca5483b47
Felizardo xavier <felizardo17@ua.pt>
2026-01-29 02:14:41 +0000

    fix6

--- demo/src/main/java/com/example/demo/controller/AuthController.java
+++ demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:3000", "http://127.0.0.1:8080"})
 public class AuthController {
 
     private final AuthService authService;
